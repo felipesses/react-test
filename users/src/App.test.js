6aeb327a6afe75d53bc067256194f9019f9daa4c
felipesses <felipeme73@gmail.com>
2023-04-17 21:26:14 -0300

    feat: finish first lesson

---
+++ users/src/App.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event'
+import App from './App';
+
+test('can receive a new user and show it on a list', async () => {
+  render(<App />)
+
+  const nameInput = screen.getByLabelText(/name/i)
+  const emailInput = screen.getByLabelText(/email/i)
+  const button = screen.getByRole('button')
+
+
+  // Simulate typing in a name
+  await user.click(nameInput);
+  await user.keyboard("jane");
+
+  // Simulate typing in an email
+  await user.click(emailInput);
+  await user.keyboard("jane@jane.com");
+
+  await user.click(button)
+
+  const name = screen.getByRole('cell', { name: 'jane' })
+  const email = screen.getByRole('cell', { name: 'jane@jane.com' })
+
+  expect(name).toBeInTheDocument()
+  expect(email).toBeInTheDocument()
+});
---
+++ users/src/UserForm.test.js
@@ -0,0 +1,64 @@
+import { render, screen } from '@testing-library/react'
+import user from '@testing-library/user-event'
+import UserForm from './UserForm'
+
+test('it shows two inputs and a button', () => {
+    render(<UserForm />)
+
+    const inputs = screen.getAllByRole('textbox')
+    const button = screen.getByRole('button')
+
+    expect(inputs).toHaveLength(2)
+    expect(button).toBeInTheDocument()
+
+})
+
+
+test("it calls onUserAdd when the form is submitted", async () => {
+    const mock = jest.fn()
+
+    // Try to render my component
+    render(<UserForm onUserAdd={mock} />);
+
+    // Find the two inputs
+    const nameInput = screen.getByLabelText(/name/i)
+    const emailInput = screen.getByLabelText(/email/i)
+
+    // Simulate typing in a name
+    await user.click(nameInput);
+    await user.keyboard("jane");
+
+    // Simulate typing in an email
+    await user.click(emailInput);
+    await user.keyboard("jane@jane.com");
+
+    // Find the button
+    const button = screen.getByRole('button')
+
+    // Simulate clicking the button
+    await user.click(button)
+
+    // Assertion to make sure 'onUserAdd' gets called with email/name
+    expect(mock).toHaveBeenCalled()
+    expect(mock).toHaveBeenCalledWith({ name: 'jane', email: 'jane@jane.com' })
+});
+
+test('empties the tow inputs when form is submitted', async () => {
+    render(<UserForm onUserAdd={() => { }} />)
+
+    const nameInput = screen.getByLabelText(/name/i)
+    const emailInput = screen.getByLabelText(/email/i)
+
+    await user.click(nameInput);
+    await user.keyboard("jane");
+
+    await user.click(emailInput);
+    await user.keyboard("jane@jane.com");
+
+    const button = screen.getByRole('button')
+
+    await user.click(button)
+
+    expect(nameInput).toHaveValue('')
+    expect(emailInput).toHaveValue('')
+})
